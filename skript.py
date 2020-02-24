8b17c18d9b061929645414837b75d76c965572f3
Kevin Striek <Kevin.Striek@gmail.com>
2020-02-24 15:14:51 +0100

    Change it so it doesn't require ffmpeg.
    And add more documentation (in German).

--- skript.py
+++ skript.py
@@ -3,6 +3,7 @@ from loguru import logger
 
 
 class MyLogger(object):
+    """Dies leitet die Ausgabe von Youtube-dl zu loguru um."""
     def debug(self, msg):
         logger.debug(msg)
 
@@ -16,7 +17,8 @@ class MyLogger(object):
         logger.info(msg)
 
 
-ydl_opts = {'format': 'bestvideo[ext=mp4][height<=720]+bestaudio[ext=m4a]/best[ext=mp4][height<=720]',
+ydl_opts = {'format': 'best[ext=mp4][height<=720]',
+            # Das bedeutet, lade das beste mp4 Video runter, was maximal 720p ist.
             'logger': MyLogger()}
 
 if __name__ == '__main__':
@@ -24,7 +26,7 @@ if __name__ == '__main__':
     link = input("Please enter it here: ")  # Dies liest den Link von der Konsole ein.
     print(f"The Link you provided is: {link}")
     print("Okay. Where should I place the file?")
-    path = input("Please enter the Path here (and add a / at the end if there is none): ")  # Dies liest den Pfad von der Konsole ein.
+    path = input("Please enter the Path here (and add a / or \\ at the end if there is none): ")  # Dies liest den Pfad von der Konsole ein.
     print(f"The path you provided is: {path}")
     ydl_opts.update({'outtmpl': f'{path}%(title)s.%(ext)s'})
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
