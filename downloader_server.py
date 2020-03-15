0019152dc6dfec9c436428825e57f51d54e4a3c9
Kevin Striek <Kevin.Striek@gmail.com>
2020-03-15 18:24:19 +0100

    Add TLS Certificate creator for downloader Server.

---
+++ downloader_server.py
@@ -0,0 +1,25 @@
+import ssl
+import anyio
+
+SERVER_CERT_CHAIN = 'FAKE_CERT_fullchain.pem'  # Change this with your server certificate's path.
+
+
+async def serve(client: anyio.SocketStream):
+    async with client:
+        name = await client.receive_until(b'\n', 1024)
+        await client.send_all(b'Hello, %s\n' % name)
+
+
+async def main():
+    # Create a context for the purpose of authenticating clients
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+
+    # Load the server certificate and private key
+    context.load_cert_chain(certfile=SERVER_CERT_CHAIN)
+
+    async with anyio.create_task_group() as tg:
+        async with await anyio.create_tcp_server(9123, ssl_context=context) as server:
+            async for client in server.accept_connections():
+                await tg.spawn(serve, client)
+
+anyio.run(main)
